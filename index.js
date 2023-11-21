04176ef92a4135baa40e107c67a89ae95c331398
eleve <eleve@example.com>
2023-11-21 16:52:27 +0100

    dockerfile

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello !");
+const http = require ('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'content-Type': 'text/plain'});
+    res.end('Hello !');
+});
+
+const port = 3000;
+server.listen (port, () => {
+    console.log('server running at http://localhost:${port}/');
+});
