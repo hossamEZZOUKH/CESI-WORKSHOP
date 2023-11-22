669c36790b143c3fa07d357f926f2190ceb96475
eleve <eleve@example.com>
2023-11-22 15:48:35 +0100

    kubernetes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require ('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'content-Type': 'text/plain'});
-    res.end('Hello !');
+    res.end('Hello, World!\n');
 });
 
 const port = 3001;
